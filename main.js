695cb0c3adc58e9d7a2e0b13d913f6d3a56136c7
GrabovyiS <graboviys@yandex.ru>
2024-03-24 17:11:38 +0300

    feat: choose custom etch-a-sketch size and reset

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 const etchASketch = document.querySelector("#etch-a-sketch");
-const SIZE = 16;
+const promptSizeButton = document.querySelector("#prompt-size-button")
+let size = 16;
+promptSizeButton.addEventListener('click', () => {
+  do {
+    if (size > 100 || size < 10) {
+      alert("Choose grid size between 10 and 100");
+    }
+    size = prompt("Grid size:");
+  }
+  while (size > 100 || size < 10)
 
-function fillEtchASketch() {
-  for (let i = 0; i < SIZE*SIZE; i++) {
+  fillEtchASketch(size);
+});
+
+fillEtchASketch(size);
+
+
+function fillEtchASketch(size) {
+  while (etchASketch.firstChild) {
+    etchASketch.removeChild(etchASketch.lastChild);
+  }
+  
+  pixelPercentSize = 100 / size - 0.2;
+  pixelPercentMargin = 0.1;
+
+  for (let i = 0; i < size * size; i++) {
     const pixelSketch = document.createElement('div');
-    pixelSketch.classList.add('sixteenth');
+    pixelSketch.style.width = pixelPercentSize + '%';
+    pixelSketch.style.height = pixelPercentSize + '%';
+    pixelSketch.style.margin = pixelPercentMargin + '%';
+
     pixelSketch.addEventListener('mouseenter', () => {
       pixelSketch.classList.add('scratched')
     });
     etchASketch.appendChild(pixelSketch);
   }
 }
-
-fillEtchASketch();
