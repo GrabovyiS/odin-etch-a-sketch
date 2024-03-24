dcbd9a6e20af679d3108159235663b98d65a7ae6
GrabovyiS <graboviys@yandex.ru>
2024-03-24 16:43:24 +0300

    feat: leave scratch mark on etch-a-sketch

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ function fillEtchASketch() {
   for (let i = 0; i < SIZE*SIZE; i++) {
     const pixelSketch = document.createElement('div');
     pixelSketch.classList.add('sixteenth');
+    pixelSketch.addEventListener('mouseenter', () => {
+      pixelSketch.classList.add('scratched')
+    });
     etchASketch.appendChild(pixelSketch);
   }
 }
